70573dc3be77077611dadd228ac873a27276d33c
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-08 18:42:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ if(mysql_select_db($dB))
   echo "Connect<br>";
 }
 else die ("Cant connect to db");
-$b = rand(1111, 9999);
-$c = date("d.m.Y");
-function generatePassword($length = 8){
+/*$b = rand(1111, 9999);
+$c = date("d.m.Y");*/
+function generatePassword($length = 100){
 $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ23456789';
 $numChars = strlen($chars);
 $string = '';
@@ -21,10 +21,7 @@ $string .= substr($chars, rand(1, $numChars) - 1, 1);
 return $string;
 }
 
-echo "Пароль из 8 символов: " . generatePassword(8) . "n";
-echo "Пароль из 8 символов: " . generatePassword(8) . "n";
-echo "Пароль из 8 символов: " . generatePassword(8) . "n";
-/*for($i=0;$i<500001;$i++)
+echo "Пароль из 8 символов: " . generatePassword(100) . "n";
 {
 
 $query = mysql_query("INSERT INTO table (a,b,c) VALUES ($a,$b,$c)");
