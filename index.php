8becf83324cdfa1c0d4444dfbc1dfe07721e5021
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-08 20:36:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -63,6 +63,15 @@ function generateDescription($length)
   }
 return $string;
 }
+function generateName($length){
+$chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ23456789';
+$numChars = strlen($chars);
+$string = '';
+for ($i = 0; $i < $length; $i++) {
+$string .= substr($chars, rand(1, $numChars) - 1, 1);
+}
+return $string;
+}
 set_time_limit(800);
 for($i = 1;$i<=3;$i++)
 {
